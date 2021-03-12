333e867e326c37dbff5b0253b1125028cf9fb0d2
Ilmira <tregulovailmira@gmail.com>
2021-03-12 14:26:36 +0200

    Refactor server

--- server/server.js
+++ server/server.js
@@ -1,23 +1,16 @@
 require('./dbMongo/mongoose');
 const http = require('http');
-const express = require('express');
-const router = require('./router');
-const cors = require('cors');
+const app = require('./app');
 const controller = require('./socketInit');
-const handlerError = require('./handlerError/handler');
 
 const PORT = process.env.PORT || 9632;
-const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use('/public', express.static('public'));
-app.use(router);
-app.use(handlerError);
 
 const server = http.createServer(app);
+
 server.listen(PORT,
   () => console.log(`Example app listening on port ${ PORT }!`));
+
 controller.createConnection(server);
 
 
