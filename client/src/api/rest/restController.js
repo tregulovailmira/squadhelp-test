181f887572ab8bf0ae491da97440508f39d3bc71
Ilmira <tregulovailmira@gmail.com>
2021-03-19 03:13:11 +0200

    Fix undefined contestId at rest controller

--- client/src/api/rest/restController.js
+++ client/src/api/rest/restController.js
@@ -9,8 +9,8 @@ export const getActiveContests = ({offset, limit, typeIndex, contestId, industry
     http.get(`contests?offset=${offset}&limit=${limit}&typeIndex=${typeIndex}&contestId=${contestId}&industry=${industry}&awardSort=${awardSort}&ownEntries=${ownEntries}`);
 export const dataForContest = ({ characteristic1, characteristic2 }) => http.get(`/contests/data?characteristic1=${characteristic1}&characteristic2=${characteristic2}`);
 export const getContestById = ({ contestId }) => http.get(`contests/${contestId}`);
-export const updateContest = data => http.put(`/contests/${data.contestId}`, data);
-export const setNewOffer = data => http.post(`/contests/${data.contestId}/offers`, data);
+export const updateContest = data => http.put(`/contests/${data.get('contestId')}`, data);
+export const setNewOffer = data => http.post(`/contests/${data.get('contestId')}/offers`, data);
 export const setOfferStatus = ({contestId, offerId, command, creatorId, orderId, priority}) => 
     http.put(`/contests/${contestId}/offers/${offerId}?command=${command}&creatorId=${creatorId}&orderId=${orderId}&priority=${priority}`);
 export const downloadContestFile = ({ fileName }) => http.get(`/contests/files/${fileName}`);
