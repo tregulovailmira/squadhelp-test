6643e35b4b6bb0b8e8cb604974a74dd7799a23ab
Ilmira <tregulovailmira@gmail.com>
2021-03-29 22:07:03 +0300

    Add render progres time at Event component

--- client/src/components/Events/EventsList/Event/index.js
+++ client/src/components/Events/EventsList/Event/index.js
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 function Event(props) {
-    const { event: { id, eventBody, eventDate,  } } = props;
-
+    const { event: { id, eventBody, eventDate, timeToStart = {} } } = props;
+    const { years, months, weeks, days, hours, minutes, seconds } = timeToStart;
     return (
         <li>
             <div>
                 {eventBody}
+            </div>
+            <div>
+                {years > 0 && <span>{years}y&nbsp;</span>}
+                {months > 0 && <span>{months}m&nbsp;</span>}
+                {weeks > 0 && <span>{weeks}w&nbsp;</span>}
+                {days > 0 && <span>{days}d&nbsp;</span>}
+                {hours > 0 && <span>{hours}h&nbsp;</span>}
+                {minutes > 0 && <span>{minutes}m&nbsp;</span>}
+                {seconds > 0 && <span>{seconds}s&nbsp;</span>}
             </div>
         </li>
     )
