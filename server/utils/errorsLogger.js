c69aff93bb043f9bb2d0f9bdaf78954d3e91be8a
Ilmira <tregulovailmira@gmail.com>
2021-04-16 18:12:16 +0300

    Create logic on adding errors to json-file

--- server/utils/errorsLogger.js
+++ server/utils/errorsLogger.js
@@ -5,57 +5,80 @@ const path = require('path');
 const mkdir = promisify(fs.mkdir);
 const writeFile = promisify(fs.writeFile);
 const access = promisify(fs.access);
+const stat = promisify(fs.stat);
 
 const logDirPath = path.resolve('log');
-
 const logFilePath = path.resolve('log', 'log.json');
 
 const createDirIfNotExist = async (path) => {
-  console.log('createDirIfNotExist');
   try {
     await access(path, fs.constants.F_OK);
-    console.log('Directory exists');
     return true;
   } catch (error) {
     if (error.code === 'ENOENT') {
-      console.log('Directory does not exists');
       await mkdir(path, { recursive: true });
-      console.log('Directory is created');
       return true;
     } else {
-      console.log('Some other error: ', error);
       throw error;
     }
   }
 };
 
 const createFileIfNotExist = async (path) => {
-  console.log('createFileIfNotExist');
   try {
     await access(path, fs.constants.F_OK);
-    console.log('File exists');
     return true;
   } catch (error) {
     if (error.code === 'ENOENT') {
-      console.log('File does not exists');
-      await writeFile(path, 'Some log\n');
-      console.log('File is created');
+      await writeFile(path, '[]');
       return true;
     } else {
-      console.log('Some other error: ', error);
       throw error;
     }
   }
 };
 
+const createObjectForLogging = (error) => {
+  return {
+    time: Date.now(),
+    message: error.message || 'Server error',
+    code: error.code || 500,
+    stackTrace: error.stack || {}
+  };
+};
+
+const getFileSize = async (filePath) => {
+  try {
+    const file = await stat(filePath);
+    return file.size;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+const addDataToLogFile = async (filePath, data, start, flags) => {
+  try {
+    const logStream = await fs.createWriteStream(filePath, { flags, start });
+    await logStream.write(data, 'utf-8');
+    logStream.end();
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
 module.exports.logErrorToFile = async (error) => {
-  console.log('logErrorToFile');
+  await createDirIfNotExist(logDirPath);
+  await createFileIfNotExist(logFilePath);
 
-  const isExistDir = await createDirIfNotExist(logDirPath);
-  console.log('isExistDir', isExistDir);
+  const preparedError = JSON.stringify(createObjectForLogging(error));
+  const fileSize = await getFileSize(logFilePath);
+  const start = fileSize - 1;
 
-  const isExistFile = await createFileIfNotExist(logFilePath);
-  console.log('isExistFile', isExistFile);
+  const dataForLog = fileSize > 2
+    ? `, ${preparedError}]`
+    : `${preparedError}]`;
 
-  return error;
+  await addDataToLogFile(logFilePath, dataForLog, start, 'r+');
 };
