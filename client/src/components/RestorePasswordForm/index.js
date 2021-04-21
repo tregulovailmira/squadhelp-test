e125b4e200f592d9b3011cd01280163febede064
Ilmira <tregulovailmira@gmail.com>
2021-04-21 21:57:56 +0300

    Add using redux state to restorePasswordForm

--- client/src/components/RestorePasswordForm/index.js
+++ client/src/components/RestorePasswordForm/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { restorePasswordAction, clearRestorePasswordAction } from '../../actions/actionCreator';
 import FormInput from '../FormInput/FormInput';
 import loginFormStyles from '../LoginForm/LoginForm.module.sass';
+import styles from './RestorePasswordForm.module.sass';
 
-const RestorePasswordForm = () => {
+const RestorePasswordForm = (props) => {
+
+    const dispatch = useDispatch();
+    const restorePassword = bindActionCreators(restorePasswordAction, dispatch);
+    const clearState = bindActionCreators(clearRestorePasswordAction, dispatch);
+
+    const { isFetching, data, error } = useSelector(state => state.restorePassword);
+    
+    const { handleSubmit } = props;
+
+    useEffect(() => {
+        return () => {
+            clearState();
+        }
+    }, []);
+
+    const submitHandler = (values) => {
+        restorePassword(values)
+    }
+    
     const formInputClasses = {
       container: loginFormStyles.inputContainer,
       input: loginFormStyles.input,
@@ -11,8 +34,18 @@ const RestorePasswordForm = () => {
       notValid: loginFormStyles.notValid,
       valid: loginFormStyles.valid,
     };
+
+    if(data && !isFetching) {
+        return (
+            <div className={styles.success}>
+                <span className="far fa-check-circle"></span>
+                <span>{data}</span>
+            </div>
+        )
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit(submitHandler)}>
             <Field name='email' 
                 classes={ formInputClasses }
                 component={ FormInput }
@@ -31,7 +64,7 @@ const RestorePasswordForm = () => {
                 label="Confirm Password"
             />
             <button type='submit' className={ loginFormStyles.submitContainer }>
-                RESTORE PASSWORD
+                {isFetching ? <span>LOADING...</span> : <span>RESTORE PASSWORD</span>}
             </button>
         </form>
     )
