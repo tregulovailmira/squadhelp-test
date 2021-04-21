e278bfbbe44b0efcbaf195eaa725a57832912928
Ilmira <tregulovailmira@gmail.com>
2021-04-22 00:43:20 +0300

    Add RestorePasswordInfo to RestorePassword page

---
+++ client/src/components/RestorePasswordInfo/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function RestorePasswordInfo() {
+
+    return (
+        <div style={{ color: 'white', textAlign: 'center', fontSize: '25px' }}>
+            Your password has been updated. Now you can login.
+        </div>
+    )
+}
--- client/src/pages/RestorePassword/index.js
+++ client/src/pages/RestorePassword/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import RestorePasswordForm from '../../components/RestorePasswordForm';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import queryString from 'query-string';
+import history from '../../browserHistory';
+import RestorePasswordForm from '../../components/RestorePasswordForm';
+import RestorePasswordInfo from '../../components/RestorePasswordInfo';
 import Logo from '../../components/Logo';
 import CONSTANTS from '../../constants';
 import styles from './RestorePassword.module.sass';
 
 const RestorePassword = () => {
+
+    const [tokenData, setTokenData] = useState('');
+    
+    useEffect(() => {
+        const { token } = queryString.parse(window.location.search);
+        setTokenData(token);
+        history.replace('/restorePassword')
+    }, [])
+
     return (
         <div className={ styles.mainContainer }>
             <div className={styles.headerContainer}>
@@ -21,7 +33,7 @@ const RestorePassword = () => {
             </div>
             <div className={styles.formContainer}>
                 <h2>RESTORE YOUR PASSWORD</h2>
-                <RestorePasswordForm />
+                {tokenData ? <RestorePasswordInfo/> : <RestorePasswordForm />}
             </div>
             
         </div>
