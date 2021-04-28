bdccc34c0dfbe478791372b5dfefb306a16311c7
Ilmira <tregulovailmira@gmail.com>
2021-04-28 22:37:55 +0300

    Add new role to constants

--- client/src/constants.js
+++ client/src/constants.js
@@ -4,6 +4,7 @@ const serverPort = env === 'production' ? 3000 : 9632
 export default {
   CUSTOMER: 'customer',
   CREATOR: 'creator',
+  MODERATOR: 'moderator',
   CONTEST_STATUS_ACTIVE: 'active',
   CONTEST_STATUS_FINISHED: 'finished',
   CONTEST_STATUS_PENDING: 'pending',
