123509846d08e3a7a2215a8f500dd8ba6b9dfca0
Ilmira <tregulovailmira@gmail.com>
2021-04-30 19:18:45 +0300

    Remove emitting new offer by creating

--- server/controllers/contestController.js
+++ server/controllers/contestController.js
@@ -136,7 +136,7 @@ module.exports.updateContest = async (req, res, next) => {
 
 module.exports.setNewOffer = async (req, res, next) => {
   const {
-    body: { contestType, offerData, contestId, customerId },
+    body: { contestType, offerData, contestId },
     tokenData,
     tokenData: { userId }
   } = req;
@@ -160,8 +160,6 @@ module.exports.setNewOffer = async (req, res, next) => {
     const createdOffer = await contestQueries.createOffer(newOffer);
     delete createdOffer.contestId;
     delete createdOffer.userId;
-
-    // controller.getNotificationController().emitEntryCreated(customerId);
 
     const User = Object.assign({}, tokenData, { id: userId });
     return res.send(Object.assign({}, createdOffer, { User }));
